12003030bc28e21836edaad65e75694d706acb11
Dmitriy Levykh <da-levikh@ya.ru>
2026-02-03 21:08:48 +0300

    Fix problems

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
     port: 5173,
   },
+  base: '/',
   resolve: {
     alias: {
       '@': '/src'
+    }
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: './index.html'
     }
   }
 })
